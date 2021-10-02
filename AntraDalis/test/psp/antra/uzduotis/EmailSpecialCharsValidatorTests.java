e7e976f008e73746b4a0bfd80539b9aae1f58a5f
Gytis Bečalis <gytis.becalis@mif.stud.vu.lt>
2021-10-02 13:48:21 +0300

    Made all access modifiers for Tests default, to be unaccessible in JAR package

--- AntraDalis/test/psp/antra/uzduotis/EmailSpecialCharsValidatorTests.java
+++ AntraDalis/test/psp/antra/uzduotis/EmailSpecialCharsValidatorTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.Assert.*;
 
-public class EmailSpecialCharsValidatorTests {
+class EmailSpecialCharsValidatorTests {
 
     EmailSpecialCharsValidator specialCharsValidator;
 
--- AntraDalis/test/psp/antra/uzduotis/PhoneValidatorAdditionalTests.java
+++ AntraDalis/test/psp/antra/uzduotis/PhoneValidatorAdditionalTests.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.Assert.*;
-
-public class PhoneValidatorAdditionalTests {
+class PhoneValidatorAdditionalTests {
 
     PhoneValidator phoneValidator;
 
